0bb6175a225f4b4a7b488750c62234dfb932a5c5
HAITIVINE <fanclone457@gmail.com>
2025-04-07 17:37:34 -0500

    Add files via upload

--- src/mobile.js
+++ src/mobile.js
@@ -1,5 +1,5 @@
 if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini/i)) {
-    alert('i detected that you are on a mobile device. this website as of now is VERY broken on mobile. please go on your pc and whatever it is anything but mobile')
+    window.location.href = 'https://haitivine.github.io/src/mobile.html';
 } else {
-    console.log("Not a mobile device.");
+    console.log("not a mobile device!");
 }
