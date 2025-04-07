e4ac816aa964ec7dd7cd4201c168babbcc25b0e4
HAITIVINE <fanclone457@gmail.com>
2025-04-07 06:39:22 -0500

    Update mobile.js

--- src/mobile.js
+++ src/mobile.js
@@ -1,5 +1,5 @@
 if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini/i)) {
-    alert('test')
+    alert('i detected that you are on a mobile device. this website as of now is VERY broken on mobile. please go on your pc and whatever it is anything but mobile')
 } else {
     console.log("Not a mobile device.");
 }
