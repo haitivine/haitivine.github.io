f15ec3a0373ea2ad4a7afd73c95078f6c5a148f8
HAITIVINE <fanclone457@gmail.com>
2025-04-27 13:56:42 -0500

    Update mobile.js

--- src/mobile.js
+++ src/mobile.js
@@ -1,5 +1,1 @@
-if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini/i)) {
-    window.location.href = 'https://haitivine.github.io/src/mobile.html';
-} else {
-    console.log("not a mobile device!");
-}
+window.location.replace("https://haitivine.neocities.org/");
